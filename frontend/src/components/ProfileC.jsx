07afef9a53cba9560c118ebf4d0f13f4b5f4b03b
Antonio Diez <idiezag@gmail.com>
2025-10-10 11:03:18 -0400

    delete message to the console

--- frontend/src/components/ProfileC.jsx
+++ frontend/src/components/ProfileC.jsx
@@ -165,7 +165,7 @@ function ProfileC({ profile, onUpdate }) {
       </div>
     );
   }
-  console.log("test");
+
   return (
     <div
       style={{ border: "1px solid #ddd", padding: "20px", borderRadius: "8px" }}
