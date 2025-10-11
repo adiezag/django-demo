a1cf7a7c1bc7ebb6d1617df247996cf7f832bbda
Antonio Diez <idiezag@gmail.com>
2025-10-11 10:17:42 -0400

    edit index.html

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-//export const N8N_WEBHOOK_URL = window.configs.VITE_N8N_WEBHOOK_URL;
+export const N8N_WEBHOOK_URL = window.configs.VITE_N8N_WEBHOOK_URL;
 // export const N8N_WEBHOOK_URL = import.meta.env.VITE_N8N_WEBHOOK_URL;
-export const N8N_WEBHOOK_URL =
-  "https://n8n.srv917216.hstgr.cloud/webhook/02f40ff6-5e6a-4b2a-a21f-46be53dd6d5c";
 
 const apiUrl = "/choreo-apis/dajngo-react-web-app/backend/v1";
 const api = axios.create({
